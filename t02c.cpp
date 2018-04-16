e4b09872d55d368af1226bb192d18ce3752eaad9
mjsiggithub <38322110+mjsiggithub@users.noreply.github.com>
2018-04-16 13:42:29 +0200

    Update t02c.cpp

--- t02c.cpp
+++ t02c.cpp
@@ -5,3 +5,19 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdio.h>
+#include <stdio.h>
+/* tdata*/
+
+sum(int x, int y);
+main()
+{
+    int a,b;
+    a = getchar();
+
+
+    b = 13;
+printf("\n");
+    printf("%d\n",sum(a-'0',b));
+   /* putchar(a);    */
+    return 0;
+}
