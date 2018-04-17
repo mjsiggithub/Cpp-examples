2089b3d4f6d88dca9f1c7bead7bbf1bbe46a902e
Marc Jumelet <38322110+mjsiggithub@users.noreply.github.com>
2018-04-17 10:01:21 +0200

    Update t02c.cpp

--- t02c.cpp
+++ t02c.cpp
@@ -21,3 +21,17 @@ printf("\n");
    /* putchar(a);    */
     return 0;
 }
+
+sum(int x, int y);
+main()
+{
+    int a,b;
+    a = getchar();
+
+
+    b = 13;
+printf("\n");
+    printf("%d\n",sum(a-'0',b));
+   /* putchar(a);    */
+    return 0;
+}
