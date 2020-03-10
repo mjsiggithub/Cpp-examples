9e411ea35cf8a6d0e8ae43eea94b91642ec56629
Marc Jumelet <38322110+mjsiggithub@users.noreply.github.com>
2020-03-10 15:48:09 +0100

    Update t02c.cpp

--- t02c.cpp
+++ t02c.cpp
@@ -11,10 +11,10 @@
 sum(int x, int y);
 main()
 {
-    int a,b;
+    int a,b,c1,c2,dasdasdasdasd;
     a = getchar();
-
-
+/* TODO: test the analyser */
+/* TODO: test the analyser */
     b = 13;
 printf("\n");
     printf("%d\n",sum(a-'0',b));
